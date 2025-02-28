dd2b5689c0310ec456d3c21f4345398b170f7911
Mason <mason.dougan16@gmail.com>
2025-02-28 01:15:45 -0600

    Start Area Fixed

--- GameDevPlatformer/Assets/Scripts/GameManager.cs
+++ GameDevPlatformer/Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using TMPro;
 using UnityEngine;
 using System;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
--- GameDevPlatformer/Assets/Scripts/MainMenu.cs
+++ GameDevPlatformer/Assets/Scripts/MainMenu.cs
@@ -3,6 +3,8 @@ using UnityEngine.SceneManagement;
 //Play button script from https://www.youtube.com/watch?v=DX7HyN7oJjE
 public class MainMenu : MonoBehaviour
 {
+
+    
     public void PlayGame(){
         SceneManager.LoadScene(1);
     }
