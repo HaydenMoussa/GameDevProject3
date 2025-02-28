e2c84976278446f61bdcdaabd8ab249bbb158b5b
Mason <mason.dougan16@gmail.com>
2025-02-28 00:40:55 -0600

    Carrot added + Triggers Credits

---
+++ GameDevPlatformer/Assets/Scripts/CarrotEnd.cs
@@ -0,0 +1,42 @@
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CarrotEnd : MonoBehaviour
+{
+
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(" Carrot Hit");
+
+
+
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log("Player hit Carrot");
+
+            PlayerController player = collision.gameObject.GetComponent<PlayerController>();
+
+            
+            SceneManager.LoadScene("Credits", LoadSceneMode.Additive);
+
+        }
+    }
+
+
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
